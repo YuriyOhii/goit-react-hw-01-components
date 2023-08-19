366cae3ad48d3a2f092de72f844e60fde5a6220b
YuriyOhii <uaogiy@gmail.com>
2023-08-19 14:28:46 +0300

    task 4 without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from '../assets/user.json';
 import data from '../assets/data.json';
 import friends from '../assets/friends.json';
+import transactions from '../assets/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 export const App = () => {
   return (
     <>
@@ -16,6 +18,7 @@ export const App = () => {
       />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </>
   );
 };
